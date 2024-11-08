148f1bdacbed8839cfcf40eb4bea21e66a228f50
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-11-08 21:36:56 +0000

    completed calculation for volume

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let input = document.getElementById('input');
 const button = document.getElementById('button');
 
 let lengthResult = document.getElementById('length-result');
-console.log(lengthResult);
-const volumeResult = document.getElementById('volume-result');
-const massResult = document.getElementById('mass-result');
+
+let volumeResult = document.getElementById('volume-result');
+let massResult = document.getElementById('mass-result');
 
 button.addEventListener('click', function () {
-  let output = `${input.value} meters = ${input.value * 3.281} feet`;
-  lengthResult.textContent += output;
+  let output = `${input.value} meters = ${(input.value * 3.281).toFixed(
+    3
+  )} feet | ${input.value} feet = ${(input.value * 0.3048).toFixed(3)} meters`;
+  lengthResult.textContent = output;
 });
