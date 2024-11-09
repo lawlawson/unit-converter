02fcd1f242f51825aae07cf9b5b44b078830bbe5
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-11-09 18:23:26 +0000

    added all calculations

--- script.js
+++ script.js
@@ -13,8 +13,22 @@ let volumeResult = document.getElementById('volume-result');
 let massResult = document.getElementById('mass-result');
 
 button.addEventListener('click', function () {
-  let output = `${input.value} meters = ${(input.value * 3.281).toFixed(
+  let lengthOutput = `${input.value} meters = ${(input.value * 3.281).toFixed(
     3
   )} feet | ${input.value} feet = ${(input.value * 0.3048).toFixed(3)} meters`;
-  lengthResult.textContent = output;
+  lengthResult.textContent = lengthOutput;
+
+  let volumeOutput = `${input.value} liters = ${(input.value * 0.264).toFixed(
+    3
+  )} gallons | ${input.value} gallons = ${(input.value * 3.7854).toFixed(
+    3
+  )} liters`;
+  volumeResult.textContent = volumeOutput;
+
+  let massOutput = `${input.value} kilos = ${(input.value * 2.204).toFixed(
+    3
+  )} pounds | ${input.value} pounds = ${(input.value * 0.453592).toFixed(
+    3
+  )} kilos`;
+  massResult.textContent = massOutput;
 });
